cd8e1b8c81466c433c1e48b659e32755c3b830de
kaline mesqita <mesquitabfkaline@gmail.com>
2025-02-02 17:57:31 -0300

    update

---
+++ map.js
@@ -0,0 +1,21 @@
+// MAP method
+const people = [
+    {
+        name:'bob',
+        age: 20,
+        position: 'developer',
+    },
+    {
+        name:'kaline',
+        age: 26,
+        position: 'engineer',
+    },
+    {
+        name:'Janine',
+        age: 32,
+        position: 'professor',
+    }
+];
+
+const ages = people.map(() =>{})
+console.log(ages)
